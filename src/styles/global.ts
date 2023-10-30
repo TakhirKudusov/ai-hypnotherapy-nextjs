68c0aa1af62047883e078db9777d975ce3816ef2
Takhir Kudusov <kudusov.takhir@gmail.com>
2023-10-30 18:51:04 +0500

    [main]: initial project config and structure

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    scroll-behavior: smooth;
+  }
+
+  html,
+  body {
+    max-width: 100vw;
+    overflow-x: hidden;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default Global;
